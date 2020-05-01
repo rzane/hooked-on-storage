4ef6097c123e8ce5fe5ce75cd1c7939ebcf01b34
Ray Zane <raymondzane@gmail.com>
2020-05-01 19:04:28 -0400

    Update the example to use the <Hydrated /> component

--- example/index.tsx
+++ example/index.tsx
@@ -1,19 +1,29 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import { createStorage } from "use-hydrated-storage";
+import { createStorage, Adapter } from "use-hydrated-storage";
+
+// Slow down local storage so we see the hydration
+const slowLocalStorage: Adapter = {
+  async getItem(key) {
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    return localStorage.getItem(key);
+  },
+  setItem(key, value) {
+    return localStorage.setItem(key, value);
+  },
+  removeItem(key) {
+    return localStorage.removeItem(key);
+  },
+};
 
 const Count = createStorage<number>({
   key: "count",
-  adapter: localStorage,
+  adapter: slowLocalStorage,
   defaultValue: 0,
 });
 
 const Counter = () => {
-  const [count, setCount, hydrated] = Count.useStorage();
-
-  if (!hydrated) {
-    return <p>Loading...</p>;
-  }
+  const [count, setCount] = Count.useStorage();
 
   return (
     <div>
@@ -26,7 +36,9 @@ const Counter = () => {
 
 ReactDOM.render(
   <Count.Provider>
-    <Counter />
+    <Count.Hydrated fallback={<p>Rehydrating...</p>}>
+      <Counter />
+    </Count.Hydrated>
   </Count.Provider>,
   document.getElementById("root")
 );
